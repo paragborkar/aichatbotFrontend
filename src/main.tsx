ad91e877f76a6cfb0e6c635c9b18267542401238
paragborkar <paragborkar200@gmail.com>
2023-10-23 09:43:48 +0530

    Routing Done Using React-Router-Dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme } from '@mui/material'
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter as Router} from 'react-router-dom'
+
+const theme = createTheme({typography : {fontFamily: "Roboto, serif", allVariants:{color:"white"}}});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Router>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
+    </Router>
   </React.StrictMode>,
 )
