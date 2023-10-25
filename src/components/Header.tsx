c202f716cbb1ce5deb5ae6899ebac5eebafad68f
paragborkar <paragborkar200@gmail.com>
2023-10-25 07:43:41 +0530

    Header Component Created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,26 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Logo from './shared/Logo';
+import { useAuth } from '../context/AuthContext';
+import NavigationLink from './shared/NavigationLink';
 
 
 const Header = () => {
+    const auth = useAuth();
   return (
     <AppBar sx={{bgcolor: "transparent" , position: "static", boxShadow: "none" }}>
         <Toolbar sx={{display:"flex"}}>
             <Logo/>
+            <div>
+                {auth?.isLoggedIn ?
+                 <> <NavigationLink bg="#00fffc" to='/chat' text="Go To Chat" textColor="Black"/>
+                    <NavigationLink bg="#51538f" textColor="white" to="/" text='logout' onClick={auth.logout} />
+                  </> : 
+                  <>
+                  <NavigationLink bg="#00fffc" to='/login' text="Login" textColor="Black"/>
+                  <NavigationLink bg="#51538f" textColor="white" to="/signup" text='Signup'/>
+                  </>}
+            </div>
         </Toolbar>
     </AppBar>
   )
